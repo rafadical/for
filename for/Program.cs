545deb475b51c43584fe47dc96e5b8a5a6081a2c
Pichau <Pichau@DESKTOP-SOV61AF>
2022-07-06 18:49:29 -0300

    Adicionar arquivos de projeto.

---
+++ for/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace @for
+{
+    internal class Program
+    {
+        static void Main(string[] args) {
+
+            int i;
+            for (i=1; i<=50; i++)
+
+
+            
+                Console.WriteLine(i);
+
+            
+
+            {
+                Console.ReadKey();
+            }
+        }
+    }
+}
